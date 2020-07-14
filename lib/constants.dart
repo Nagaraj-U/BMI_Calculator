03c539b558a7f3fb30607ab5478e839fea0c5523
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-07-14 13:21:32 +0530

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kTextStyle=TextStyle(
+    color: Color(0xFF8D8E98),
+    fontSize: 18
+);
+
+
+const Color kActiveCardColor=Color(0xFF1D1E33);
+const Color kInactiveCardColor=Color(0xFF111328);
+const Color kBottomContainerColor=Color(0xFFEB1555);
+const double kBottomContainerHeight=70.0;
+
+const kHeightTextStyle=TextStyle(
+    fontSize: 50,
+    fontWeight: FontWeight.w900
+);
